3944c56bb597fd1e5358186ec48c7374ac6a78e8
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-08-28 12:39:34 +0300

    init Hibernate configuration

---
+++ src/main/java/org/example/config/SessionFactoryImpl.java
@@ -0,0 +1,25 @@
+package org.example.config;
+
+import org.example.model.Game;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryImpl {
+
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration();
+
+        configuration.setProperty("hibernate.connection.Driverclass", "org.postgresql.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/gameshop");
+        configuration.setProperty("hibernate.connection.username", "postgres");
+        configuration.setProperty("hibernate.connection.password", "postgres");
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.format_sql", "true");
+
+        configuration.addAnnotatedClass(Game.class);
+
+        return configuration.buildSessionFactory();
+    }
+}
--- src/main/java/org/example/model/Game.java
+++ src/main/java/org/example/model/Game.java
@@ -1,20 +1,43 @@
 package org.example.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "games")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Game {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "title", nullable = false)
     private String title;
+
+    @Column(name = "genre", nullable = false)
     private String genre;
+
+    @Column(name = "price", nullable = false)
     private double price;
+
+    @Column(name = "rating")
     private double rating;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "releaseDate", nullable = false)
     private LocalDate releaseDate;
+
+    @Column(name = "addedDate", nullable = false)
     private LocalDate addedDate;
 }
